7f62766c6904be00232db9e6a4490d1f3aa0dc2f
MarioMajila <mariustshipichik@gmail.com>
2018-12-12 11:43:17 +0100

    Initialisation des variables

---
+++ renderer.js
@@ -0,0 +1,27 @@
+// Initialisation des variables
+// Zones d'affichages
+const input = document.getElementById("input")
+const inputop = document.getElementById("inputop")
+
+// Chiffres
+const bouton0 = document.getElementById("zero")
+const bouton1 = document.getElementById("un")
+const bouton2 = document.getElementById("deux")
+const bouton3 = document.getElementById("trois")
+const bouton4 = document.getElementById("quatre")
+const bouton5 = document.getElementById("cinq")
+const bouton6 = document.getElementById("six")
+const bouton7 = document.getElementById("sept")
+const bouton8 = document.getElementById("huit")
+const bouton9 = document.getElementById("neuf")
+const virgule = document.getElementById("virgule")
+
+// Opérations
+const egal = document.getElementById("egal")
+const plus = document.getElementById("plus")
+const moins = document.getElementById("moins")
+const mult = document.getElementById("mult")
+const div = document.getElementById("div")
+const racine = document.getElementById("sqrt")
+const puiss = document.getElementById("pow")
+const cleaner = document.getElementById("clean")
